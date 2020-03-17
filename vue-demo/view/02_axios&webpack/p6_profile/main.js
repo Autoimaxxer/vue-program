7d599f44833030a729ac0550f78dcd66ca252738
AutoImaxxer <214582123@qq.com>
2020-03-17 15:49:51 +0800

    webpack

--- vue-demo/view/02_axios&webpack/p6_profile/main.js
+++ vue-demo/view/02_axios&webpack/p6_profile/main.js
@@ -1,5 +1,8 @@
 import Vue from '../../../js/vue.js';
 import App from './App.js';
+import Cal from './Cal.js';
+console.log('abc');
+console.log(Cal);
 
 new Vue({
     el:'#app',
---
+++ vue-demo/view/02_axios&webpack/p6_profile/webpack.pro.config.js
@@ -0,0 +1,11 @@
+module.exports={
+    //入口
+    entry:{
+        //可以有多个入口，也可以有一个，如果是一个，就默认从这个入口开始分析
+        'main':'./main.js'
+    },
+    //输出
+    output:{
+        filename:'./build.js'
+    },
+}
