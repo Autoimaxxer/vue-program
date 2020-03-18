974a0b7d0e1963992f4837430085990d22b04ac5
AutoImaxxer <214582123@qq.com>
2020-03-18 10:01:57 +0800

    webpack config img

--- vue-demo/view/02_axios&webpack/p7_css/webpack.dev.config.js
+++ vue-demo/view/02_axios&webpack/p7_css/webpack.dev.config.js
@@ -14,7 +14,10 @@ module.exports = {
     module: {
         loaders: [//webpack后面版本可以叫做roles
             { test: /\.css$/, loader: 'style-loader!css-loader' },
-            {test:/\.(jpg|png|gif|svg)$/,loader:'url-loader?limit=200'}
+            {test:/\.(jpg|png|gif|svg)$/,loader:'url-loader?limit=4096'}
+            //如果文件大于limit，则生成一个文件
+            //如果小于limit，则生产base64到build.js中
+            //建议比较小的图片，使用base64的方式
         ]
     },
 
