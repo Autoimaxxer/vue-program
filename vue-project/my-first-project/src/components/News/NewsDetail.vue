dbcdfe4bcb9aed74900d7bf402aab4e616743265
AutoImaxxer <214582123@qq.com>
2020-04-17 17:06:41 +0800

    change weixin axios account

--- vue-project/my-first-project/src/components/News/NewsDetail.vue
+++ vue-project/my-first-project/src/components/News/NewsDetail.vue
@@ -17,6 +17,13 @@ export default {
     // 这里获取地址栏参数是 route，不是router
     console.log(this.$route.query.id)
     this.id = this.$route.query.id
+    this.$axios.get('https://weixin.ytzq.com/servlet/json', {
+      params: { weixinpk: 'gh_9a6fe4a350e3', openid: 'oheoxs-KkM5N2FKVvYolifOeGNwA', funcNo: '5001009' }
+    }).then(function (res) {
+      console.log(res)
+    }).catch(function (res) {
+      console.log(res)
+    })
   }
 }
 </script>
