d8c1d3d8b59436e36a1f938243e6079987ffd1b8
AutoImaxxer <214582123@qq.com>
2020-05-07 16:18:12 +0800

    change news

--- vue-project/my-first-project/src/components/News/NewsDetail.vue
+++ vue-project/my-first-project/src/components/News/NewsDetail.vue
@@ -17,8 +17,16 @@ export default {
     // 这里获取地址栏参数是 route，不是router
     console.log(this.$route.query.id)
     this.id = this.$route.query.id
-    this.$axios.get('https://weixin.ytzq.com/servlet/json', {
-      params: { weixinpk: 'gh_9a6fe4a350e3', openid: 'oheoxs-KkM5N2FKVvYolifOeGNwA', funcNo: '5001009' }
+    const sid = this.$route.query.sid
+    // this.$axios.get('https://weixin.ytzq.com/servlet/json', {
+    //   params: { weixinpk: 'gh_9a6fe4a350e3', openid: 'oheoxs-KkM5N2FKVvYolifOeGNwA', funcNo: '5001009' }
+    // }).then(function (res) {
+    //   console.log(res)
+    // }).catch(function (res) {
+    //   console.log(res)
+    // })
+    this.$axios.get('/apiopenjk/getSingleJoke', {
+      params: { sid: sid }
     }).then(function (res) {
       console.log(res)
     }).catch(function (res) {
